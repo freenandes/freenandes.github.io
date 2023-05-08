f2e96fb2c379d76f3edf77db34de242561d27bd1
freenandes <42041153+freenandes@users.noreply.github.com>
2023-05-08 21:00:08 +0100

    Testing multiple collections

---
+++ src/content/2nd/outlier.md
@@ -0,0 +1,5 @@
+---
+title: "Outlier"
+---
+### Shameless
+This folk was excluded from the party.
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,19 @@
-// 1. Import utilities from `astro:content`
+// Import utilities from `astro:content`
 import { defineCollection, z } from 'astro:content';
-// 2. Define your collection(s)
-const blogCollection = defineCollection({
-    schema: z.object({
-        title: z.string(),
-      }),
-});
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
+
+// Define your collection(s)
+// const blogCollection = defineCollection({ });
+
+// Export a single `collections` object to register your collection(s). This key should match your collection directory name in "src/content"
 export const collections = {
-  '1st': blogCollection,
+  '1st': defineCollection({
+    schema: z.object({
+      title: z.string(),
+    }),
+  }),
+  '2nd': defineCollection({
+    schema: z.object({
+      title: z.string(),
+    }),
+  }),
 };

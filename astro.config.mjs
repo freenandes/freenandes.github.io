f486b5648637324655f5ebcdd8bf5d653270faa0
freenandes <freenandes@gmail.com>
2023-08-30 23:28:05 +0100

    Updated Astro, improved zooming

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://freenandes.github.io',
-    experimental: {
-        assets: true,
-    },
 });
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,2 +1,3 @@
 declare module 'mark.js';
 declare module 'lozad';
+declare module 'tippy.js/dist/tippy.esm.js';
